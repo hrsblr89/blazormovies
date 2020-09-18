55ec363da4122d28021460ee515179fb881b09e9
hrsblr89 <hrs.blr89@gmail.com>
2020-09-19 01:17:50 +0530

    after lecture 45

--- BlazorApp3/Client/Pages/Counter.razor.cs
+++ BlazorApp3/Client/Pages/Counter.razor.cs
@@ -11,49 +11,15 @@ namespace BlazorApp3.Client.Pages
 {
     public partial class Counter
     {
-        [Inject]
-        SingletonService singleton { get; set; }
-        [Inject] 
-        TransientService transient { get; set; }
-        [Inject]
-        IJSRuntime js { get; set; }
-
-        [CascadingParameter]
-        public AppState state { get; set; }
-        
+              
 
         private int currentCount = 0;
-        private static int currentCountStatic = 0;
-
-        private List<Movie> movies;
-        protected override void OnInitialized()
-        {
-            movies = new List<Movie>()
-            {
-                new Movie() { Title = "Joker", ReleaseDate = new DateTime(2019, 7, 2) },
-                new Movie() { Title = "Avengers", ReleaseDate = new DateTime(2016, 11, 23) },
-            };
-        }
-
-        [JSInvokable]
-        public async Task IncrementCount()
+       
+        public void IncrementCount()
         {
             currentCount++;
-            currentCountStatic++;
-            transient.value = currentCount;
-            singleton.value = currentCount;
-            await js.InvokeVoidAsync("dotnetStaticInvocation");
+       
         }
 
-        [JSInvokable]
-        public static Task<int> GetCurrentCount()
-        {
-            return Task.FromResult(currentCountStatic);
-        }
-
-        private async Task IncrementCountJavaScript()
-        {
-            await js.InvokeVoidAsync("dotnetInstanceInvocation", DotNetObjectReference.Create(this));
-        }
     }
 }
--- BlazorApp3/Client/Program.cs
+++ BlazorApp3/Client/Program.cs
@@ -26,8 +26,6 @@ namespace BlazorApp3.Client
         private static void ConfigureServices(IServiceCollection services)
         {
             services.AddOptions(); // needed for authorization
-            services.AddSingleton<SingletonService>();
-            services.AddTransient<TransientService>();
             services.AddScoped<IRepository, RepositoryInMemory>();
         }
     }
